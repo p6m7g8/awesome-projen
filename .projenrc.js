dae467ccc3a5fb74c41feccbc5cebcf1a193398e
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-03-26 13:32:52 -0400

    feat: updates to p6-projen-project-awesome-list@1.0.1 (#338)

--- .projenrc.js
+++ .projenrc.js
@@ -1,104 +1,14 @@
 const { AwesomeList } = require('p6-projen-project-awesome-list');
-
 const project = new AwesomeList({
   author: 'Philip M. Gollucci',
   authorAddress: 'pgollucci@p6m7g8.com',
-  devDeps: ['p6-projen-project-awesome-list'],
+  defaultReleaseBranch: 'main',
+  devDeps: ['p6-projen-project-awesome-list@1.0.1'],
   name: 'awesome-projen',
-  repositoryUrl: 'https://github.com/p6m7g8/awesome-projen.git',
+  repositoryUrl: 'https://github.com/pgollucci/awesome-projen.git',
 
-  /* AwesomeListProjectOptions */
-  // contactEmail: undefined,                                                  /* What e-mail address to list for the Code of Conduct Point of Contact. */
-
-  /* JsiiProjectOptions */
-  // compat: false,                                                            /* Automatically run API compatibility test against the latest version published to npm after compilation. */
-  // compatIgnore: '.compatignore',                                            /* Name of the ignore file for API compatibility tests. */
-  // docgen: true,                                                             /* Automatically generate API.md from jsii. */
-  // dotnet: undefined,                                                        /* Publish to NuGet. */
-  // eslint: true,                                                             /* Install eslint. */
-  // java: undefined,                                                          /* Publish to maven. */
-  // python: undefined,                                                        /* Publish to pypi. */
-  // rootdir: '.',                                                             /* undefined */
-
-  /* NodePackageOptions */
-  // allowLibraryDependencies: true,                                           /* Allow the project to include `peerDependencies` and `bundledDependencies`. */
-  // authorEmail: undefined,                                                   /* Author's e-mail. */
-  // authorName: undefined,                                                    /* Author's name. */
-  // authorOrganization: undefined,                                            /* Author's Organization. */
-  // authorUrl: undefined,                                                     /* Author's URL / Website. */
-  // autoDetectBin: true,                                                      /* Automatically add all executables under the `bin` directory to your `package.json` file under the `bin` section. */
-  // bin: undefined,                                                           /* Binary programs vended with your module. */
-  // bundledDeps: undefined,                                                   /* List of dependencies to bundle into this module. */
-  // deps: [],                                                                 /* Runtime dependencies of this module. */
-  // description: undefined,                                                   /* The description is just a string that helps people understand the purpose of the package. */
-  // entrypoint: 'lib/index.js',                                               /* Module entrypoint (`main` in `package.json`). */
-  // homepage: undefined,                                                      /* Package's Homepage / Website. */
-  // keywords: undefined,                                                      /* Keywords to include in `package.json`. */
-  // license: 'Apache-2.0',                                                    /* License's SPDX identifier. */
-  // licensed: true,                                                           /* Indicates if a license should be added. */
-  // maxNodeVersion: undefined,                                                /* Minimum node.js version to require via `engines` (inclusive). */
-  // minNodeVersion: undefined,                                                /* Minimum Node.js version to require via package.json `engines` (inclusive). */
-  // npmTaskExecution: NpmTaskExecution.PROJEN,                                /* Determines how tasks are executed when invoked as npm scripts (yarn/npm run xyz). */
-  // packageManager: NodePackageManager.YARN,                                  /* The Node Package Manager used to execute scripts. */
-  // packageName: undefined,                                                   /* The "name" in package.json. */
-  // peerDependencyOptions: undefined,                                         /* Options for `peerDeps`. */
-  // peerDeps: [],                                                             /* Peer dependencies for this module. */
-  // projenCommand: 'npx projen',                                              /* The shell command to use in order to run the projen CLI. */
-  // repository: undefined,                                                    /* The repository is the location where the actual code for your package lives. */
-  // repositoryDirectory: undefined,                                           /* If the package.json for your package is not in the root directory (for example if it is part of a monorepo), you can specify the directory in which it lives. */
-  // scripts: {},                                                              /* npm scripts to include. */
-  // stability: undefined,                                                     /* Package's Stability. */
-
-  /* NodeProjectOptions */
-  // antitamper: true,                                                         /* Checks that after build there are no modified files on git. */
-  // buildWorkflow: undefined,                                                 /* Define a GitHub workflow for building PRs. */
-  // codeCov: false,                                                           /* Define a GitHub workflow step for sending code coverage metrics to https://codecov.io/ Uses codecov/codecov-action@v1 A secret is required for private repos. Configured with @codeCovTokenSecret. */
-  // codeCovTokenSecret: undefined,                                            /* Define the secret name for a specified https://codecov.io/ token A secret is required to send coverage for private repositories. */
-  // copyrightOwner: undefined,                                                /* License copyright owner. */
-  // copyrightPeriod: undefined,                                               /* The copyright years to put in the LICENSE file. */
-  // defaultReleaseBranch: 'master',                                           /* The name of the main release branch. */
-  // dependabot: true,                                                         /* Include dependabot configuration. */
-  // dependabotOptions: undefined,                                             /* Options for dependabot. */
-  // gitignore: undefined,                                                     /* Additional entries to .gitignore. */
-  // jest: true,                                                               /* Setup jest unit tests. */
-  // jestOptions: undefined,                                                   /* Jest options. */
-  // libdir: 'lib',                                                            /* Compiler artifacts output directory. */
-  // mergify: true,                                                            /* Adds mergify configuration. */
-  // mergifyAutoMergeLabel: 'auto-merge',                                      /* Automatically merge PRs that build successfully and have this label. */
-  // mergifyOptions: undefined,                                                /* Options for mergify. */
-  // npmDistTag: 'latest',                                                     /* The dist-tag to use when releasing to npm. */
-  // npmignore: undefined,                                                     /* Additional entries to .npmignore. */
-  // npmignoreEnabled: true,                                                   /* Defines an .npmignore file. Normally this is only needed for libraries that are packaged as tarballs. */
-  // npmRegistry: 'registry.npmjs.org',                                        /* The registry url to use when releasing packages. */
-  // projenDevDependency: true,                                                /* Indicates of "projen" should be installed as a devDependency. */
-  // projenUpgradeAutoMerge: undefined,                                        /* Automatically merge projen upgrade PRs when build passes. */
-  // projenUpgradeSchedule: [ '0 6 * * *' ],                                   /* Customize the projenUpgrade schedule in cron expression. */
-  // projenUpgradeSecret: undefined,                                           /* Periodically submits a pull request for projen upgrades (executes `yarn projen:upgrade`). */
-  // projenVersion: Semver.latest(),                                           /* Version of projen to install. */
-  // pullRequestTemplate: true,                                                /* Include a GitHub pull request template. */
-  // pullRequestTemplateContents: undefined,                                   /* The contents of the pull request template. */
-  // rebuildBot: undefined,                                                    /* Installs a GitHub workflow which is triggered when the comment "@projen rebuild" is added to a pull request. */
-  // rebuildBotCommand: 'rebuild',                                             /* The pull request bot command to use in order to trigger a rebuild and commit of the contents of the branch. */
-  // releaseBranches: [ 'master' ],                                            /* Branches which trigger a release. */
-  // releaseEveryCommit: true,                                                 /* Automatically release new versions every commit to one of branches in `releaseBranches`. */
-  // releaseSchedule: undefined,                                               /* CRON schedule to trigger new releases. */
-  // releaseToNpm: false,                                                      /* Automatically release to npm when new versions are introduced. */
-  // releaseWorkflow: undefined,                                               /* Define a GitHub workflow for releasing from "master" when new versions are bumped. */
-  // srcdir: 'src',                                                            /* Typescript sources directory. */
-  // testdir: 'test',                                                          /* Tests directory. */
-  // workflowBootstrapSteps: 'yarn install --frozen-lockfile && yarn projen',  /* Workflow steps to use in order to bootstrap this repo. */
-  // workflowContainerImage: undefined,                                        /* Container image to use for GitHub workflows. */
-  // workflowNodeVersion: undefined,                                           /* The node version to use in GitHub workflows. */
-
-  /* ProjectOptions */
-  // clobber: true,                                                            /* Add a `clobber` task which resets the repo to origin. */
-  // gitpod: false,                                                            /* Adds a gitpod configuration. */
-  // outdir: '.',                                                              /* The root directory of the project. */
-  // parent: undefined,                                                        /* The parent project, if this project is part of a bigger project. */
-  // projectType: ProjectType.UNKNOWN,                                         /* Which type of project this is (library/app). */
-  // readme: undefined,                                                        /* The README setup. */
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // packageName: undefined,  /* The "name" in package.json. */
 });
-
-project.gitignore.exclude('.node-version');
-
 project.synth();
---
+++ API.md
@@ -0,0 +1,39 @@
+# API Reference <a name="API Reference" id="api-reference"></a>
+
+
+
+## Classes <a name="Classes" id="Classes"></a>
+
+### Hello <a name="Hello" id="awesome-projen.Hello"></a>
+
+#### Initializers <a name="Initializers" id="awesome-projen.Hello.Initializer"></a>
+
+```typescript
+import { Hello } from 'awesome-projen'
+
+new Hello()
+```
+
+| **Name** | **Type** | **Description** |
+| --- | --- | --- |
+
+---
+
+#### Methods <a name="Methods" id="Methods"></a>
+
+| **Name** | **Description** |
+| --- | --- |
+| <code><a href="#awesome-projen.Hello.sayHello">sayHello</a></code> | *No description.* |
+
+---
+
+##### `sayHello` <a name="sayHello" id="awesome-projen.Hello.sayHello"></a>
+
+```typescript
+public sayHello(): string
+```
+
+
+
+
+
